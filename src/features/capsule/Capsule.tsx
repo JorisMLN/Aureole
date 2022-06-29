ee3c167b8c7788224bc3ad0afab6dbfd6eacddeb
Joris Monleon <monleon.joris@gmail.com>
2022-06-29 10:24:04 +0200

    Maj archi (subCapsule)

--- src/features/capsule/Capsule.tsx
+++ src/features/capsule/Capsule.tsx
@@ -1,12 +1,20 @@
 import './capsule.scss';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
+import SubCapsule from '../subCapsule/SubCapsule';
+import Test from '../testComponent/TestComponent';
 
 const Capsule = () => {
   return (
     <>
       <div className='capsule'>
         <Header />
+
+        <SubCapsule child={<Test />}/>
+        <SubCapsule child={<Test />}/>
+        <SubCapsule child={<Test />}/>
+        <SubCapsule child={<Test />}/>
+
         <Footer />
       </div>
     </>
---
+++ src/features/testComponent/TestComponent.tsx
@@ -0,0 +1,11 @@
+// import './header.scss';
+
+const Test = () => {
+  return(
+    <>
+      <div className="test"> test </div>
+    </>
+  )
+}
+
+export default Test;
